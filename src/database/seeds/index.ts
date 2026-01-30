913431326329d191a90f829bf2dcd3ad678e39f1
Danilo Martinez <danilomartinezferreira@gmail.com>
2026-01-29 20:30:18 -0400

    chore: remove verbose logs from seeds

--- src/database/seeds/index.ts
+++ src/database/seeds/index.ts
@@ -3,11 +3,7 @@ import { seedMedicine } from "./seedMedicines.js";
 import { seedUsers } from "./seedUsers.js";
 
 export function runSeeds() {
-  console.log("Running database seeds...");
-
   seedDoctors();
   seedMedicine();
   seedUsers();
-
-  console.log("Database seeds finished");
 }
